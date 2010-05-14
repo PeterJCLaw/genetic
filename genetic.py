fa1733c6b986dce23a4c1de1debb2fa4466a65ae
Sam Phippen <samphippen@googlemail.com>
2010-05-14 14:49:13 +0100

    make all gl code work

--- genetic.py
+++ genetic.py
@@ -1,4 +1,10 @@
-from random import randint
+from random import randint, seed
+from pyglet import gl
+from pyglet.graphics import OrderedGroup, Batch
+from pyglet import window
+from time import sleep
+
+import pyglet
 
 def clamp(x, lower, upper):
     if x > upper:
@@ -22,6 +28,7 @@ class Triangle:
         for x,y in self.points:
             result.append(x)
             result.append(y)
+        return result
 
     def serialize_color(self):
         return self.color[0:]
@@ -31,11 +38,12 @@ class Triangle:
         new_green = clamp(self.color[1]+randint(-delta,delta),0,255);
         new_blue = clamp(self.color[2]+randint(-delta,delta),0,255);
         new_alpha = clamp(self.color[3]+randint(-delta,delta),0,255);
+        self.color = [new_red, new_green, new_blue, new_alpha]
         self.has_changed = True
     
     def generate(self, xmax, ymax):
         self.points = [0]*3
-        for i in range(0,3):
+        for i in xrange(0,3):
             x,y = randint(0,xmax),randint(0,ymax)
             self.points[i] = (x,y)
         self.color[0] = randint(0,255)
@@ -45,9 +53,9 @@ class Triangle:
         self.has_changed = True
 
     def reshape_delta(self, xmax, ymax, delta):
-        for i in range(0, randint(0,2)):
+        for i in xrange(0, randint(0,2)):
             choice = randint(0,len(self.points)-1)
-            x,y = randint(-delta,delta)
+            x,y = randint(-delta,delta), randint(-delta, delta)
             x += self.points[choice][0]
             y += self.points[choice][1]
             x = clamp(x, 0, xmax)
@@ -56,7 +64,87 @@ class Triangle:
         self.has_changed = True
 
 class Drawing:
-    def __init__(self, width, height):
+    def __init__(self, width, height, batch):
         self.width = width
         self.height = height
-     
+        self.triangles = []
+        self.batch = batch
+        self.batch.add(4,gl.GL_QUADS,OrderedGroup(0),("v2i/static", (0,0,0,height,width,height,0,height)), ("c3B/static",[0,0,0]*4))
+        
+         
+    def mutate(self, num_mutations):
+        triangles = self.triangles
+        for i in xrange(0, num_mutations):
+            e = randint(0,2)
+            if e == 0:
+                choice = randint(0, len(triangles)-1)
+                triangles[choice].recolor_self_delta(5)
+                self.vertex_list.vertices[choice*6:(choice*6)+6] = triangles[choice].serialize_points()
+                self.vertex_list.colors[choice*12:choice*12+12] = triangles[choice].serialize_color()*3
+            else:
+                choice = randint(0, len(triangles)-1)
+                triangles[choice].reshape_delta(self.width, self.height, 25)
+                self.vertex_list.vertices[choice*6:(choice*6)+6] = triangles[choice].serialize_points()
+                self.vertex_list.colors[choice*12:choice*12+12] = triangles[choice].serialize_color()*3
+            elif e == 2:
+                c1 = randint(0, len(triangles)-1)
+                c2 = clamp(c1 + randint(-5,5), 0, len(triangles)-1)
+                triangles[c1],triangles[c2] = triangles[c2],triangles[c1]
+                
+               
+    
+    def draw(self):
+        vertices = []
+        color = []
+        vertex_list = self.vertex_list
+         
+        self.batch.draw()
+
+    def generate(self, number_triangles):
+        vertices = []
+        colors = []
+        for i in xrange(0, number_triangles):
+            t = Triangle()
+            t.generate(self.width, self.height)        
+            t.has_changed = False
+            self.triangles.append(t)
+
+            vertices.extend(t.serialize_points())
+            colors.extend(t.serialize_color()*3)
+        print len(colors), len(self.triangles)*3*4
+        self.vertex_list = self.batch.add(
+            number_triangles*3, gl.GL_TRIANGLES, OrderedGroup(1), 
+            ("v2i/stream", vertices),
+            ("c4B/stream", colors)
+        )
+
+        for i in xrange(0, number_triangles):
+            self.vertex_list.vertices[i*6:(i*6)+6] = self.triangles[i].serialize_points()
+            self.vertex_list.colors[i*12:(i*12)+12] = self.triangles[i].serialize_color()*3
+
+
+def main():
+    width = 400
+    height = 400
+    b = Batch()
+    d = Drawing(width,height, b)
+    d.generate(100)
+    w = window.Window(width,height,"cows", vsync = False)
+    w.set_visible(True)
+    gl.glEnable(gl.GL_BLEND)
+    gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
+        
+    fps = pyglet.clock.ClockDisplay()
+    @w.event
+    def on_draw():
+        w.clear()
+        d.draw()
+        fps.draw()
+        a = (gl.GLuint * (3*d.width*d.height))(0)
+        gl.glReadPixels(0, 0, d.width, d.height, gl.GL_RGB, gl.GL_UNSIGNED_INT, a)
+
+    pyglet.clock.schedule(lambda x:d.mutate(3))
+    pyglet.app.run()
+if __name__ == "__main__":
+    seed(3)
+    main()
