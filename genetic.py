7df5d508bdbf0f1bf827ec3a74b803cb70474dcb
Sam Phippen <samphippen@googlemail.com>
2010-05-17 16:11:31 +0100

    remove random profiling quit

--- genetic.py
+++ genetic.py
@@ -307,8 +307,6 @@ def main():
         else:
             newdrawing = olddrawing
         i += 1
-        if (i == 20000):
-            pyglet.app.exit()
         
         if (i % 20 == 0):
             w.set_caption(str(fps.get_fps())+" "+str(parentdiff) + " " + str(log(parentdiff,10))+ " " + str(i))
